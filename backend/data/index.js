5154feb213e1b7ec70cefd7f8fec1696bf0e2daf
Kavit Shah <kas24698@gmail.com>
2023-04-09 22:35:43 -0400

    created basic structure of data files

--- backend/data/index.js
+++ backend/data/index.js
@@ -1,3 +1,13 @@
-module.exports = {
+const companyData = require('./company');
+const userData = require('./user');
+const projectData = require('./project');
+const stateData = require('./state');
+const ticketData = require('./ticket');
 
+module.exports = {
+    company : companyData,
+    user : userData,
+    project : projectData,
+    state : stateData,
+    ticket : ticketData
 }
