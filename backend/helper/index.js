71403f582c912e3c07b5df168f5322a7b73e451f
Kavit Shah <kas24698@gmail.com>
2023-04-09 22:26:26 -0400

    created basic structure of helper files

--- backend/helper/index.js
+++ backend/helper/index.js
@@ -1,4 +1,15 @@
+const commonHelpers = require('./common');
+const companyHelpers = require('./company');
+const userHelpers = require('./user');
+const projectHelpers = require('./project');
+const stateHelpers = require('./state');
+const ticketHelpers = require('./ticket');
 
 module.exports = {
-
+    common : commonHelpers,
+    company : companyHelpers,
+    user : userHelpers,
+    project : projectHelpers,
+    state : stateHelpers,
+    ticket : ticketHelpers
 }
