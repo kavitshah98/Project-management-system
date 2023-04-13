15353b2137010bb242eac5527cadc6ba1511902e
Kavit Shah <kas24698@gmail.com>
2023-04-13 02:23:53 -0400

    created helper functions and constants

--- backend/constant.js
+++ backend/constant.js
@@ -1,3 +1,20 @@
+const PRIORITY = ['HIGH', 'MEDIUM', 'LOW'];
+
+const TICKET_TYPE = ['STORY', 'TASK', 'BUG'];
+
+const ROLE = ['ADMIN', 'MANAGER', 'DEVELOPER', 'QA', 'SUPPORT'];
+
+const REQUIRE_FIELD_FOR_TICKET_CREATION = ["companyId", "projectId", "type", "priority", "name", "stateId", "creator", "description"];
+
+const FORBIDDEN_FIELD_FOR_TICKET_CREATION = ["closeDate", "reopenDate"];
+
+const FORBIDDEN_FIELD_FOR_TICKET_UPDATE = ["companyId", "projectId", "type", "creator", "sprintId"];
+
 module.exports = {
-    
+    PRIORITY,
+    TICKET_TYPE,
+    ROLE,
+    REQUIRE_FIELD_FOR_TICKET_CREATION,
+    FORBIDDEN_FIELD_FOR_TICKET_CREATION,
+    FORBIDDEN_FIELD_FOR_TICKET_UPDATE
 }
--- backend/helper/ticket.js
+++ backend/helper/ticket.js
@@ -1,3 +1,118 @@
-module.exports = {
+const common = require("./common");
+const constant = require("../constant");
 
+const isValidTicketPriority = (priority) => {
+    priority = common.isValidString(priority, "ticketPriority");
+
+    for(let i=0;i<constant.PRIORITY.length;i++)
+        if(priority.toLowerCase() === constant.PRIORITY[i].toLowerCase())
+            return constant.PRIORITY[i];
+    throw {status: '400', error : "Invalid ticket priority"};
+}
+
+const isValidTicketType = (type) => {
+    type = common.isValidString(type, "ticketType");
+
+    for(let i=0;i<constant.TICKET_TYPE.length;i++)
+        if(type.toLowerCase() === constant.TICKET_TYPE[i].toLowerCase())
+            return constant.TICKET_TYPE[i];
+    throw {status: '400', error : "Invalid ticket type"};
+}
+
+const isValidWatchers = (watchers) => {
+    if(!Array.isArray(watchers))
+            throw {status: '400', error : `Invalid data type for watchers`};
+    for(let i=0;i<watchers.length;i++)
+    {            
+        watchers[i] = common.isValidEmail(watchers);
+    }
+    return watchers;    
+}
+
+const isValidDependedOnTickets = (dependedOnTickets) => {
+    if(!Array.isArray(dependedOnTickets))
+            throw {status: '400', error : `Invalid data type for dependedOnTickets field`};
+    for(let i=0;i<dependedOnTickets.length;i++)
+    {            
+        dependedOnTickets[i] = common.isValidId(dependedOnTickets);
+    }
+    return dependedOnTickets;    
+}
+
+const isValidTicketUpdateData = (data) =>{
+    for(let i=0;i<constant.FORBIDDEN_FIELD_FOR_TICKET_UPDATE;i++)
+        if(data[constant.FORBIDDEN_FIELD_FOR_TICKET_UPDATE[i]])
+            throw {status: '400', error : `Invalid ${constant.FORBIDDEN_FIELD_FOR_TICKET_UPDATE[i]} field`};
+    data = isValidTicketData(data);
+}
+
+const isValidTicketCreationData = (data) =>{
+    for(let i=0;i<constant.REQUIRE_FIELD_FOR_TICKET_CREATION;i++)
+        if(!data[constant.REQUIRE_FIELD_FOR_TICKET_CREATION[i]])
+            throw {status: '400', error : `Require ${constant.REQUIRE_FIELD_FOR_TICKET_CREATION[i]} field`};
+    for(let i=0;i<constant.FORBIDDEN_FIELD_FOR_TICKET_CREATION;i++)
+        if(data[constant.FORBIDDEN_FIELD_FOR_TICKET_CREATION[i]])
+            throw {status: '400', error : `Invalid ${constant.FORBIDDEN_FIELD_FOR_TICKET_CREATION[i]} field`};
+    data = isValidTicketData(data);
+}
+
+const isValidTicketData = (data) =>{
+    for(key in data)
+    {
+        switch(key){
+            case "companyId":
+                data.email = common.isValidId(data.companyId);
+                break;
+            case "projectId":
+                data.projectId = common.isValidId(data.projectId);
+                break;
+            case "type":
+                data.type = isValidTicketType(data.type);
+                break;
+            case "priority":
+                data.priority = isValidTicketPriority(data.priority);
+                break;
+            case "sprintId":
+                data.sprintId = common.isValidId(data.sprintId);
+                break;
+            case "name":
+                data.name = common.isValidString(data.name);
+                break;
+            case "stateId":
+                data.stateId = common.isValidId(data.stateId);
+                break;
+            case "creator":
+                data.creator = common.isValidEmail(data.creator);
+                break;
+            case "assign":
+                data.assign = common.isValidEmail(data.assign);
+                break;
+            case "watchers":
+                data.watchers = isValidWatchers(data.watchers);
+                break;
+            case "expectedDate":
+                data.expectedDate = common.isValidFutureDate(data.expectedDate);
+                break;
+            case "reopenDate":
+                data.reopenDate = common.isValidPastDate(data.reopenDate);
+                break;
+            case "closeDate":
+                data.closeDate = common.isValidPastDate(data.closeDate);
+                break;
+            case "dependedOnTickets":
+                data.dependedOnTickets = isValidDependedOnTickets(data.dependedOnTickets);
+                break;
+            case "description":
+                data.description = common.isValidString(data.description);
+                break;
+            default:
+                throw {status: '400', error : `Invalid key - ${key}`};
+            
+        }
+    }
+    return data;
+}
+module.exports = {
+    isValidTicketUpdateData,
+    isValidTicketCreationData
 };
