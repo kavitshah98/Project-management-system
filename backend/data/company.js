ee5353c9e1f0d9dfea41362b3e9db0f773814105
Kavit Shah <kas24698@gmail.com>
2023-04-13 03:59:20 -0400

    createCompany data and route wihtout error checking

--- backend/data/company.js
+++ backend/data/company.js
@@ -1,3 +1,42 @@
-module.exports = {
+const mongoCollections = require('../config/mongoCollections');
+const companies = mongoCollections.company;
 
+const bcryptjs = require('bcryptjs');
+
+const {ObjectId} = require('mongodb');
+
+const saltRounds = 10;
+
+const getCompanyById = async (id) => {
+    //id = commonHelper.isValidId(id);
+
+    const companyCollection = await companies();
+    const company = await companyCollection.findOne({_id: new ObjectId(id)},{projection:{hashedPassword:0}});
+    if (company === null) throw {status:"404",error:'No company with that id'};
+    company['_id']=company['_id'].toString()
+    return company;
+  };
+
+const createCompany = async (email,EIN,name,password) => {
+    let hashedPassword = await bcryptjs.hash(password,saltRounds);
+    let newCompany = {
+        email,
+        EIN,
+        name,
+        hashedPassword
+    }
+    const companyCollection = await companies(); 
+    const insertInfo = await companyCollection.insertOne(newCompany);
+    if (!insertInfo.acknowledged || !insertInfo.insertedId)
+    throw {status:"400",error:'Could not add Company'};
+
+    const newCompanyId = insertInfo.insertedId.toString();
+    return await getCompanyById(newCompanyId);
+}
+
+
+
+module.exports = {
+    createCompany,
+    getCompanyById
 };
--- backend/routes/company.js
+++ backend/routes/company.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const data = require("../data");
+const helper = require('../helper');
+const companyData = data.company;
+const xss = require('xss');
+
 
 //Maggie
 router
- .route('/user')
+ .route('/')
  .post(async (req, res) => {
-
+    try{
+        const bodyData = req.body;
+        for(let i in bodyData)
+          bodyData[i]=xss(bodyData[i])
+        // bodyData.email=commonHelper.isValidEmail(bodyData.email);
+        // bodyData.age = patientHelper.isValidAge(bodyData.age);
+        // bodyData.name=commonHelper.isValidName(bodyData.name);
+        
+        // bodyData.zip=commonHelper.isValidZip(bodyData.zip);
+        // bodyData.password=commonHelper.isValidPassword(bodyData.password);
+        
+        let newCompany = await companyData.createCompany(bodyData.email,bodyData.EIN,bodyData.name,bodyData.password);
+        if(newCompany){
+          res.json(newCompany);
+        } else throw {status:401,error:'Could not create company'};
+      }catch(e){
+        if(typeof e !== 'object' || !('status' in e))
+          res.status(500).json("Internal server error");
+        else
+          res.status(parseInt(e.status)).json(e.error);
+        return;
+      }
  })
 
 //Maggie
