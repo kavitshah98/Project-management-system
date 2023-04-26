9726ed3d629f57b27b405dd259622bbd21dfabac
Kavit Shah <kas24698@gmail.com>
2023-04-25 20:42:23 -0400

    Update email.js

--- backend/service/email.js
+++ backend/service/email.js
@@ -2,7 +2,7 @@ const {transporter} = require("../config/nodeMailerTransporter");
 
 const sendEmail = async(to, subject, html) => {
     await transporter.sendMail({
-        from: '<doctolib.app@gmail.com>',
+        from: '<workmate.webapp@gmail.com>',
         to,
         subject,
         html
