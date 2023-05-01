39077e57d47ce212d018a7c45e992f666a641884
Kavit Shah <kas24698@gmail.com>
2023-04-30 23:05:58 -0400

    Create and update sprint

--- frontend/src/pages/project/[projectId]/sprint/[sprintId].js
+++ frontend/src/pages/project/[projectId]/sprint/[sprintId].js
@@ -1,8 +1,114 @@
 import React from 'react'
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { api } from "../../../../api";
+import { helper } from "../../../../helper";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 
 const Sprint = () => {
+
+  const [sprintData, setSprintData] = useState('');
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [hasError, setHasError] = useState(false);
+  const [error, setError] = useState('')
+  const [hasSuccessMessage, setHasSuccessMessage] = useState(false);
+  const router = useRouter();
+
+  useEffect(()=>{
+    const fetchData = async () =>{
+      try{
+        const sprintDataTemp = await api.project.getSprintById(router.query.projectId, router.query.sprintId);
+        console.log(sprintDataTemp.data);
+        setSprintData(sprintDataTemp.data)
+        setName(sprintDataTemp.data.name)
+        setDescription(sprintDataTemp.data.description);
+        setStartDate(sprintDataTemp.data.startDate);  
+      }catch(e){
+        if(e.response.status===500)
+            router.push("/error");
+        else if(e.response.status===401 )
+        {
+            localStorage.clear();
+            router.push("/login");
+        }else{
+            setHasError(true);
+            setError(e.response.data);
+        }
+      }
+    }
+    if(!sprintData){
+      fetchData();
+    }
+  },[])
+
+  const handleInputChange = (e) => {
+    const sprintDataTemp = { ...sprintData };
+    if(hasSuccessMessage)
+        setHasSuccessMessage(false);
+    if(hasError)
+        setError(false);
+    if (e.target.id === "sprintName") 
+      sprintDataTemp.name = e.target.value;
+    else if (e.target.id === "sprintDescription")
+      sprintDataTemp.description = e.target.value;
+    // else if (e.target.id === "sprintStartDate")
+    //   sprintDataTemp.startDate = e.target.value;
+    setSprintData(sprintDataTemp);
+  }
+
+  const validateUpdate = async (e) =>{
+    e.preventDefault();
+    try{
+      //console.log(sprintData);
+      setName(helper.validationFunctions.isValidSprintName(sprintData.name));
+      setDescription(helper.validationFunctions.isValidString(sprintData.description, 'Description'));
+      setStartDate(helper.validationFunctions.isValidDate(sprintData.startDate));
+      setHasError(false);
+    }catch(e){
+      setHasError(true);
+      setError(e.message);
+      return;
+    }
+
+    try{
+      const data = {"name" : sprintData.name ,"description" : sprintData.description , "startDate" : sprintData.startDate}
+      console.log(data)
+      const response = await api.project.updateSprint(router.query.projectId, router.query.sprintId ,data)
+      console.log(response)
+      setSprintData(response.data);
+      setHasError(false);
+      setHasSuccessMessage(true);
+    }catch(e){
+      console.log(e)
+      setHasError(true);
+      if(!e.response) setError("Error");
+      else setError(e.response.data);
+      return;
+    }
+  }
   return (
-    <div>Sprint</div>
+    <div>
+    {hasSuccessMessage && <div className='successMessage'>Successfully updated</div>}
+    {hasError && <div className="error">{error}</div>}
+       {sprintData && <div  className='container'>
+            <form onSubmit={validateUpdate}>
+                <br/>
+                <div className="sprintInputField"> <label className="sprintInputText" htmlFor="sprintName"> Name : </label> <input placeholder="Sprint Name" id="sprintName" value={sprintData.name} onChange={handleInputChange} type="text" className="sprintInput" autoFocus/></div>
+                <br/>
+                <label  htmlFor='sprintStartDate'>Start Date : </label>
+                <DatePicker className="sprintInputField" id='sprintStartDate' name="sprintStartDate" selected={new Date(sprintData.startDate)} onChange={(date)=>setSprintData({...sprintData, startDate: date})} />
+                <br/>
+                <div className="sprintInputField"> <label className="sprintInputText" htmlFor="sprintDescription"> Description : </label> <textarea placeholder="Description" id="sprintDescription" value={sprintData.description} onChange={handleInputChange} type="text" className="sprintInput" autoFocus/></div>
+                <br/>
+                <button type="submit" className="updateSprintButton">Update Sprint
+                </button>
+            </form>
+            </div>}
+    </div>
   )
 }
 
--- frontend/src/pages/project/[projectId]/sprint/create-sprint.js
+++ frontend/src/pages/project/[projectId]/sprint/create-sprint.js
@@ -1,8 +1,101 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { api } from "../../../../api";
+import { helper } from "../../../../helper";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const CreateSprint = () => {
-  return (
-    <div>CreateSprint</div>
+
+  const [sprintData, setSprintData] = useState({});
+  const [projectData, setprojectData] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const [error, setError] = useState('')
+  const router = useRouter();
+  const { projectId } = router.query;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try{
+        const {data: projectDataTemp} = await api.project.getProjectById(projectId);
+        setprojectData(projectDataTemp);
+      }catch(e){
+        if(e.response.status===500)
+            router.push("/error");
+          else if(e.response.status===401 )
+          {
+            router.push("/login");
+          }else{
+            setHasError(true);
+            setError(e.response.data);
+          }
+      }
+    }
+    fetchData();
+  }, []);
+
+  const handleInputChange = (e) => {
+    const sprintDataTemp = { ...sprintData };
+    if (e.target.id === "sprintName") 
+      sprintDataTemp.name = e.target.value;
+    else if (e.target.id === "sprintDescription")
+      sprintDataTemp.description = e.target.value;
+    else if (e.target.id === "sprintStartDate")
+      sprintDataTemp.startDate = e.target.value;
+    setSprintData(sprintDataTemp);
+  };
+
+  const validateSprint = async (e) =>{
+    e.preventDefault();
+    try
+    {
+        setSprintData(helper.validationFunctions.isValidSprintData(sprintData));
+    }catch(e){
+        console.log(e);
+        setHasError(true);
+        setError(e.message);
+        return;
+    }
+    
+    try{
+        const data = helper.validationFunctions.isValidSprintData(sprintData);
+        await api.project.createSprint(projectId, data);
+        alert("Sprint Created");
+        router.push(`/project/${projectId}/sprint`);
+        setHasError(false);
+    }catch(e){
+        //console.log(e);
+        setHasError(true);
+        setError(e.response.data);
+        return;
+    }
+}
+
+    return (
+    <div className='sprintPage'>
+        {projectData &&  sprintData && sprintData.length!=0 && projectData && projectData.length!=0 ?    
+        <div className="sprintCard" id="sprintFormWrap">    
+          <h1>Sprint</h1>
+          <form onSubmit={validateSprint} id="sprintForm">
+            <label htmlFor='sprintName'>Name : </label>
+            <input value={sprintData.name} className="SprintInput" id='sprintName' placeholder="Enter Sprint Name" name="sprintName" type="text" onChange={handleInputChange}/>
+            <br/>
+            <label htmlFor='sprintDescription'>Description : </label>
+            <textarea value={sprintData.description} className="SprintInput" id='sprintDescription' placeholder="Enter Sprint Description" name="sprintDescription" type="text" onChange={handleInputChange}/>
+            <br/>
+            <label  htmlFor='sprintStartDate'>Start Date : </label>
+            <DatePicker selected={sprintData.startDate ? new Date(sprintData.startDate) : new Date()} className="SprintInput" id='sprintStartDate' name="sprintStartDate" onChange={(date)=>setSprintData({...sprintData, startDate: date})}/>
+            <br/>
+            <button type="submit" className="createSprintButton">Create Sprint</button>
+              
+          </form>
+          {hasError && <div className="error">{error}</div>}
+        </div>
+        : projectData !== null && projectData.length==0 ?
+        <div>Please create project to create sprint</div>
+        :
+        <div>Loading....</div>}
+    </div>
   )
 }
 
