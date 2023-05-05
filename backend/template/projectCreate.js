995578d8c2b94e676ca49ec38e6a4a840112907b
Kavit Shah <kas24698@gmail.com>
2023-05-05 18:03:52 -0400

    email template folder

---
+++ backend/template/projectCreate.js
@@ -0,0 +1,19 @@
+export const projectCreate = `
+
+  <body>
+    <div style="background-color: #f5f5f5; padding: 20px">
+      <h1>New Project Created</h1>
+      <p>Hello,</p>
+      <p>A new project has been created. The project details are as follows:</p>
+      <ul>
+        <li><strong>Project Name:</strong> ${PROJECT_NAME}</li>
+        <li><strong>Project Description:</strong> ${PROJECT_DESCRIPTION}</li>
+        <li><strong>Project Manager:</strong> ${PROJECT_MANAGER}</li>
+        <li><strong>Project Members:</strong> ${PROJECT_MEMBERS}</li>
+      </ul>
+      <p>You can view the project details by logging in to our platform.</p>
+      <p>Thank you,</p>
+      <p>The ${COMPANY_NAME} Team</p>
+    </div>
+  </body>
+`;
