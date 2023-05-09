985f42d1a59e38a965f3a9fb6f126ef0ba070de2
Kavit Shah <kas24698@gmail.com>
2023-05-09 12:21:45 -0400

    readme with new seed files

--- backend/dbFile/comment.js
+++ backend/dbFile/comment.js
@@ -4,31 +4,19 @@ const comment = [{
   "_id": new ObjectId("645a488105401d99d12760ea"),
   "sender": "developer1.company1@gmail.com",
   "ticketId": "645a486a05401d99d12760e9",
-  "timeStamp": {
-    "$date": {
-      "$numberLong": "1683638401975"
-    }
-  },
+  "timeStamp": new Date("1683648793236"),
   "text": "Hello"
 },{
   "_id": new ObjectId("645a488e05401d99d12760eb"),
   "sender": "developer1.company1@gmail.com",
   "ticketId": "645a486a05401d99d12760e9",
-  "timeStamp": {
-    "$date": {
-      "$numberLong": "1683638414455"
-    }
-  },
+  "timeStamp": new Date("1683648793236"),
   "text": "Please finish this on time"
 },{
   "_id": new ObjectId("645a4aa405401d99d12760ee"),
   "sender": "developer1.company1@gmail.com",
   "ticketId": "645a49a305401d99d12760ed",
-  "timeStamp": {
-    "$date": {
-      "$numberLong": "1683638948821"
-    }
-  },
+  "timeStamp": new Date("1683648793236"),
   "text": "hello"
 }];
 
