14a4c4be440c399b1f986ad0f4a5cf6d686bf451
Kavit Shah <kavitshah98@yahoo.com>
2025-10-20 23:23:36 -0400

    add api url for react app

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,16 @@
 import axios from "axios";
 
+const API_BASE_URL = 
+  process.env.REACT_APP_API_URL || 
+  process.env.VITE_API_URL || 
+  "http://localhost:3000";
+
 export const axiosAuth = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: API_BASE_URL,
 });
 
 export const axiosNoAuth = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: API_BASE_URL,
 });
 
 axiosAuth.interceptors.request.use((config) => {
